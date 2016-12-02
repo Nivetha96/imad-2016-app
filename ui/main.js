7893e7d40655d8f6f6f42209a5352adbc63b3f20
Nivetha96 <nivethabalsu@gmail.com>
2016-12-02 21:29:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-var content=document.getElementById("content").value;
+var content=document.getElementById("content");
 var button=document.getElementById("button");
 button.onclick=function(){
     content.innerHTML="WELCOME TO MY WEBPAGE";
