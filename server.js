b6c5c7bfed23f3676673bd46f52b27a85f7d4ddc
Nivetha96 <nivethabalsu@gmail.com>
2016-12-02 20:05:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles={
-    'article_one':{
+    'article-one':{
     title: "article-one",
     heading: "ARTICLE-ONE",
     content:
@@ -14,7 +14,7 @@ var articles={
 <h1>hello</h1>
 </div>`
 },
-'article_two':{
+'article-two':{
     title: "article-two",
     heading: "ARTICLE-TWO",
     content:
@@ -22,7 +22,7 @@ var articles={
 <h1>hello</h1>
 </div>`
 },
- 'article_three':{
+ 'article-three':{
     title: "article-three",
     heading: "ARTICLE-THREE",
     content:
@@ -57,9 +57,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:article_name',function(req,res){
-    var article_name=req.params.article_name;
-    res.send(createTemplate(articles[article_name]));
+app.get('/:articleName',function(req,res){
+    var articleName=req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 
