8939ddfd525c8303e36c380df8855600321fc97e
Nivetha96 <nivethabalsu@gmail.com>
2016-12-03 12:17:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ button.onclick=function(){
     content.innerHTML="WELCOME TO MY WEBPAGE";
 };
 var i=document.getElementById("madi");
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+100;
+    i.style.marginLeft=marginLeft+'px';
+}
 i.onclick=function(){
-  i.style.marginLeft="100px";  
+var interval=setInterval(moveRight,100);    
+ // i.style.marginLeft="100px";  
 };
