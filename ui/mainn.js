fecfcd85dcc64e0d89aff6f6276cc37f1146d1b7
Nivetha96 <nivethabalsu@gmail.com>
2016-12-09 22:04:35 +0530

    [imad-console] Updates ui/mainn.js

--- ui/mainn.js
+++ ui/mainn.js
@@ -8,13 +8,13 @@ var n=document.getElementById("name").value;
           if(request.status===200){
     var names=request.responseText;
     names=JSON.parse(names);
-    var list='';
+    var lists='';
     for(var i=0;i<names.length;i++){
-        list +='<li>'+names[i]+'</li>';
+        lists +='<li>'+names[i]+'</li>';
         
     }
     var ul=document.getElementById("namelist");
-    ul.innerHTML=list;     
+    ul.innerHTML=lists;     
           }
       }  
     };
