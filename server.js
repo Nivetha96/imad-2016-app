c6325c448d23c5067c7366fcd543523f6b46a6e6
Nivetha96 <nivethabalsu@gmail.com>
2016-12-09 19:48:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 var names=[];
-app.get('/submit/:name',function(req,res){
+app.get('/ui/personal.html/submit/:name',function(req,res){
     var name=req.params.name;
     
     names.push(name);
